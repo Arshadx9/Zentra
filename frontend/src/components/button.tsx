f4a95836e77c9731f7d59db63a477219da7cd2df
name <arshadalib007@gmail.com>
2026-02-22 09:59:18 +0530

    changes button styling

--- frontend/src/components/button.tsx
+++ frontend/src/components/button.tsx
@@ -8,8 +8,16 @@ interface Buttonprops {
 }
  
 const variantStyles = {
-    "primary": "bg-purple-600 text-white",
-    "secondary": "bg-purple-400 text-white"
+    primary: `bg-gradient-to-r from-zinc-800 via-zinc-900 to-zinc-700
+ 
+            border-zinc-500
+            ring-1 ring-inset ring-white/5
+            text-white
+            focus:outline-none focus:ring-2 focus:ring-zinc-400/30
+            shadow-lg shadow-black/40
+            font-medium rounded-lg text-sm px-4 py-2.5 leading-5
+            transition-all duration-300`,
+    "secondary": `bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-base text-sm px-4 py-2.5 text-center text-white leading-5`
 }
 
 const sizeStyles = {
