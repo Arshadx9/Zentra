272e89f3358ad17b9b9eb21b2ab0a3a8ecafcf8c
name <arshadalib007@gmail.com>
2026-02-28 13:34:46 +0530

    connected query to pinecone and added embeddings logic

--- backend/src/query.ts
+++ backend/src/query.ts
@@ -1,6 +1,18 @@
+import { OpenAIEmbeddings } from "@langchain/openai"
+import { Pinecone } from "@pinecone-database/pinecone"
+
 export const queryPDf = () => {
 
 
+const pincone = new Pinecone({
+    apiKey:process.env.PINECONE_API_KEY!
+})
 
-    
+const index = pincone.index(process.env.PINECONE_INDEX!)
+
+const embeddings = new OpenAIEmbeddings({
+    apiKey : process.env.OPENAI_API_KEY!,
+    model:"text-embedding-3-small"
+})
+
 }
