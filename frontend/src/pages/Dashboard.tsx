15c6b286d27105559b10cc67cc9a82e451b83d9d
name <arshadalib007@gmail.com>
2026-02-28 22:26:30 +0530

    added the multi-tenant logic

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,11 +1,34 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Button } from '../components/button'
 import { Card } from '../components/card'
 import { Createpop } from '../components/contentpop'
 import { useContent } from '../hooks/useContent'
 import { Fileupload } from '../components/fileupload'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
+
 
 export const Dashboard = () => {
+
+  const [upload , setUpload] = useState()
+  const token = localStorage.getItem("token")
+  const navigate = useNavigate
+
+  useEffect( () => {
+axios.get("http://localhost:3001/uploads" , {
+
+headers: {
+  authorization:token
+}
+
+})
+.then((response)=>{
+  setUpload(response.data.uploads)
+})
+
+  },[] )
+
+
   const [modalOpen, setModalOpen] = useState(false)
   const contents = useContent()
 
@@ -24,6 +47,17 @@ export const Dashboard = () => {
           <Card key={index} title={title} link={link} />
         ))}
       </div>
+
+{upload.map( (upload)=>{
+<div key={upload._id}>
+
+  <p>{upload.Originalname} </p>
+  <button onClick={() => navigate(`/chat/${upload._id}`)} >Ask question </button>
+
+</div>
+}
+)}
+
     </div>
   )
 }
